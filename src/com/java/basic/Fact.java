307909afb0d0050cdf9ba214b7f17b5179dbe1a6
dheeraj004 <daj619@gmail.com>
2020-09-23 13:47:24 +0530

    changes

--- src/com/java/basic/Fact.java
+++ src/com/java/basic/Fact.java
@@ -1,17 +1,33 @@
 package com.java.basic;
 
+import java.util.Scanner;
+
 public class Fact {
 
 	public static void main(String[] args) {
+		int num;
+		System.out.println("Enter a Number");
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
 
-		int num=4;
-		int fact=1;
-		for(int i=1;i<=num;i++)
-		{
-			fact=num*i;
-		}
-		System.out.println(fact);
+		System.out.println(fact(num));
 
 	}
 
+	public static int fact(int num)
+	{
+		
+		int f=1;
+		if(num==0)
+		{
+			return 1;
+		}
+		for(int i=1;i<=num;i++)
+		{
+			f=f*i;
+		}
+		return f;
+		
+		
+	}
 }
--- src/com/java/basic/SwapNumber.java
+++ src/com/java/basic/SwapNumber.java
@@ -15,15 +15,29 @@ public class SwapNumber {
 		 */
 		
 		
-		int a=10,b=20, temp;
+		/*
+		 * int a=10,b=20, temp; System.out.println("Value of A before swaping:"+a+"\n"+
+		 * "Value of B before swaping:"+b);
+		 * 
+		 * temp=a; b=a; b=temp; System.out.println("Value of A after swaping:"+a+
+		 * "\n"+"Value of B after swaping:"+b);
+		 */
+		
+		
+		int a=10,b=20;
 		System.out.println("Value of A before swaping:"+a+"\n"+
 		"Value of B before swaping:"+b);
 		
-		temp=a;
-		b=a;
-		b=temp;
+		a=a*b;
+		b=a/b;
+		a=a/b;
+		
+		System.out.println("----------------------------");
 		System.out.println("Value of A after swaping:"+a+
 				 "\n"+"Value of B after swaping:"+b);
+		
+		
+		
 		
 		
 	}
